d1723e1b463a07b93e87bc51d8688894557353d7
Sean-Tay <sean@u.nus.edu>
2015-03-27 13:24:22 +0800

    First Commit?

---
+++ desktop/src/com/mygdx/Test/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.Test.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.Test.Test;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Test(), config);
+	}
+}
